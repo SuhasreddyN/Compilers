56b5d0f04895a77922548b465c136f2e90447123
pskalip <46084448+pskalip@users.noreply.github.com>
2021-05-26 18:12:49 +0530

    Add files via upload

---
+++ Lexer/test.c
@@ -0,0 +1,48 @@
+// Group Number 37
+/*2017B4A70885H   Suhas Reddy N
+2017B3A71048H   Shanmukh Kali Prasad
+2017B3A70373H   Pranav Sai Itapu
+2017B3A70381H   Vashist SLN
+*/
+int str= [123,123,453,"abc"];
+
+int fact( int n)
+{
+	if (n <= 1)
+		return 1;
+	else 
+								return n*fact(n-1);
+int x=-2;
+x-|2;
+}
+
+void main(void)
+{
+
+	float y=3.14?;
+	char s[]= " hello world";     //hello strimg
+	char s1[] = "hello     ";      
+	char s2[] = "	hello
+	world";
+	int a = 0569;
+	int b = 0000;
+
+	int abs = +123;
+	int x=-4;
+	float smt = 0.0;
+	float smt1 = 4.5;
+	float z=+0.012;
+	float y=12.12.21;
+	x = 1;
+	x= x/-2;
+	
+	while (x <= 10)
+	{
+		write(x);
+		write(fact(x));
+		writeln();
+		int 4n;
+		x = x + 1;
+	}
+}
+
